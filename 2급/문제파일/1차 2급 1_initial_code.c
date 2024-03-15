7f4e343683349da687f3620ec8661eb491b6aa43
RiverGang <sory304@naver.com>
2024-03-15 17:28:34 +0900

    Update 1차 2급 1_initial_code.c

--- 2급/문제파일/1차 2급 1_initial_code.c
+++ 2급/문제파일/1차 2급 1_initial_code.c
@@ -2,10 +2,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
-int* solution(char* shirt_size[], int shirt_size_len) {
-    // Write code here.
-    int* answer;
+#include <string.h>
+int* solution(char* shirt_size[], int shirt_size_len) { // 출력 int 주소
+    
+    int* answer = (int*)malloc(sizeof(int) * 6)
+        for (int i = 0; i < shirt_size_len; i++)
+            answer[i] = 0;
+        
+    for (int i = 0; i < shirt_size_len; i++)
+        if (strcmp(shirt_size[0], "XS") == 0)
+            answer[0]++;
+        else if (strcmp(shirt_size[1], "S") == 0)
+            answer[1]++;
+        else if (strcmp(shirt_size[1], "M") == 0)
+            answer[2]++;
+        else if (strcmp(shirt_size[1], "L") == 0)
+            answer[3]++;
+        else if (strcmp(shirt_size[1], "XL") == 0)
+            answer[4]++;
+        else if (strcmp(shirt_size[1], "XXL") == 0)
+            answer[5]++;
+        
     return answer;
 }
 
